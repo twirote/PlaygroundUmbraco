89c49cb0a8af3dd1797d759fe61318390c36af9c
wirot.t <wirot.t@softdebut.com>
2025-07-04 14:46:30 +0700

    Initial home page

---
+++ Views/Partials/image/view.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PlaygroundUmbraco.Views.Partials.image
+{
+    public class viewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
